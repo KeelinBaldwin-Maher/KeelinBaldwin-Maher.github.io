f63a47df7d841fd1004f3d42638656092dbaaba6
Keelin <keelin.baldwinmaher@gmail.com>
2024-06-12 12:05:03 -0600

    Mobile navigation styles and javascript
    Styling navigation for mobile pages. Used media query breakpoint. Added a mobile navigation container to hold hamburger menu and logo.
    Added a change event listener to mediaQueryList, a MediaQueryList object. The scroll event listener is removed when the window matches the query.
    Also added a click event listener to hamburger menu button.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,4 @@
-
-const navBar = document.querySelector("nav");
-
+const navBar = document.querySelector("#primary-navigation");
 let prevScrollPos = 0;
 // https://andrewwalpole.com/blog/the-showy-hidey-nav-bar/
 function toggleNav() {
@@ -11,11 +9,9 @@ function toggleNav() {
     const section2Top = (sections[1]) ? sections[1].offsetTop : 0;
     const section3Top = (sections[2]) ? sections[2].offsetTop : 0;
 
-    if (st > prevScrollPos && st > navBar.clientHeight && 
-        !(st > (section2Top - 100) && st < section2Top) && 
+    if (st > prevScrollPos && st > navBar.clientHeight &&
+        !(st > (section2Top - 100) && st < section2Top) &&
         st !== section3Top) {
-        console.log(st);
-        console.log(section2Top);
         navBar.classList.remove("show");
         navBar.classList.add("hide");
 
@@ -25,7 +21,6 @@ function toggleNav() {
     }
     prevScrollPos = st;
 }
-
 
 function activeSection() {
     const sections = document.querySelectorAll("section");
@@ -46,18 +41,58 @@ function activeSection() {
 
     const activeSection = visibleSections[0];
 
+    // If there is a active link, then remove the 'active' style
     const currentActiveLink = document.querySelector("nav .active");
-    
     (currentActiveLink) ? currentActiveLink.classList.remove("active") : "";
 
+    // All the links have data-name values that match the section they link to
     document.querySelector(`nav a[data-name="${activeSection.id}"]`).classList.add("active");
 }
 
-activeSection();
-
-window.addEventListener('scroll', onScroll);
-
-function onScroll() {
+function handleScroll() {
     toggleNav();
     activeSection();
 }
+
+const mediaQueryWidth = window.matchMedia("(max-width: 40em)");
+
+if (!mediaQueryWidth.matches) {
+    activeSection();
+    window.addEventListener('scroll', handleScroll);
+}
+
+function handleMediaQueryWidth(event) {
+    if (!event.matches) {
+        // navBar.classList.remove("hide");
+        // navBar.classList.add("show");
+        activeSection();
+        window.addEventListener('scroll', handleScroll);
+        
+    } else {
+        // Reset nav style
+        const currentActiveLink = document.querySelector("nav .active");
+        (currentActiveLink) ? currentActiveLink.classList.remove("active") : "";
+        // navBar.classList.remove("show");
+        // navBar.classList.add("hide");
+
+        window.removeEventListener('scroll', handleScroll);
+    }
+}
+
+mediaQueryWidth.addEventListener("change", handleMediaQueryWidth);
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+function handleHamburgerMenu() {
+    if (hamburgerMenu.ariaExpanded === "true") {
+        // Close menu
+        hamburgerMenu.ariaExpanded = "false";
+    } else {
+        // Open menu
+        hamburgerMenu.ariaExpanded = "true";
+
+    }
+}
+
+hamburgerMenu.addEventListener("click", handleHamburgerMenu)
+
