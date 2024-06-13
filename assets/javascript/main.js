4bdddb8c4336af14f8f68bf35669472e6e010fbd
Keelin <keelin.baldwinmaher@gmail.com>
2024-06-12 19:15:22 -0600

    Mobile nav styling complete
    Mobile nav button open and closes with animation. Mobile nav closes when a link is clicked.
    Hamburger button has aria-controls, aria-expanded.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,6 +9,7 @@ function toggleNav() {
     const section3Top = (sections[2]) ? sections[2].offsetTop : 0;
 
     if (st > prevScrollPos && st > navBar.clientHeight &&
+        prevScrollPos !== 0 &&
         !(st > (section2Top - 100) && st < section2Top) &&
         st !== section3Top) {
         navBar.classList.remove("show");
@@ -30,7 +31,7 @@ function activeSection() {
     for (let i = 0; i < sections.length; i++) {
         // Get the dimensions of the section. 
         const sectionRect = sections[i].getBoundingClientRect();
-        
+
         // The top and bottom attributes will be negative if they are not visible 
         const visibilityPercent = Math.floor((sectionRect.bottom / windowHeight) * 100);
 
@@ -48,53 +49,85 @@ function activeSection() {
     document.querySelector(`nav a[data-name="${activeSection.id}"]`).classList.add("active");
 }
 
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenuSpans = document.querySelectorAll(".hamburger-menu-icon span");
+
+function handleHamburgerMenu() {
+    if (hamburgerMenu.ariaExpanded === "true") {
+        // Close menu
+        hamburgerMenu.ariaExpanded = "false";
+
+        hamburgerMenuSpans[0].style.animation = "hamburger-span-1-reverse 0.3s ease 0s 1 normal forwards";
+        hamburgerMenuSpans[1].style.opacity = "1";
+        hamburgerMenuSpans[2].style.animation = "hamburger-span-3-reverse 0.3s ease 0s 1 normal forwards";
+
+        navBar.classList.remove("show");
+        navBar.classList.add("hide");
+    } else {
+        // Open menu
+        hamburgerMenu.ariaExpanded = "true";
+
+        hamburgerMenuSpans[0].style.animation = "hamburger-span-1 0.3s ease 0s 1 normal forwards";
+        hamburgerMenuSpans[1].style.opacity = "0";
+        hamburgerMenuSpans[2].style.animation = "hamburger-span-3 0.3s ease 0s 1 normal forwards";
+
+        navBar.classList.remove("hide");
+        navBar.classList.add("show");
+    }
+}
+
+hamburgerMenu.addEventListener("click", handleHamburgerMenu);
+
 function handleScroll() {
     toggleNav();
     activeSection();
 }
 
-const mediaQueryWidth = window.matchMedia("(max-width: 40em)");
+const mediaQueryWidth = window.matchMedia("(min-width: 50em)");
 
-if (!mediaQueryWidth.matches) {
-    activeSection();
-    window.addEventListener('scroll', handleScroll);
+// if (mediaQueryWidth.matches) {
+//     activeSection();
+//     window.addEventListener('scroll', handleScroll);
+// }
+
+function closeMobileNavMenu() {
+    hamburgerMenu.ariaExpanded = "false";
+    hamburgerMenuSpans[0].style.animation = "hamburger-span-1-reverse 0.3s ease 0s 1 normal forwards";
+    hamburgerMenuSpans[1].style.opacity = "1";
+    hamburgerMenuSpans[2].style.animation = "hamburger-span-3-reverse 0.3s ease 0s 1 normal forwards";
+    navBar.classList.remove("show");
 }
 
 function handleMediaQueryWidth(event) {
-    if (!event.matches) {
-        // navBar.classList.remove("hide");
-        // navBar.classList.add("show");
+    const navMenuLinks = document.querySelectorAll("nav .menu-link");
+
+    if (event.matches) { // Mobile nav off
+        // Reset hamburger
+        hamburgerMenu.ariaExpanded = "false"
+        hamburgerMenuSpans[0].style.animation = "";
+        hamburgerMenuSpans[1].style.opacity = "1";
+        hamburgerMenuSpans[2].style.animation = "";
+        // Reset nav
+        navBar.classList.remove("hide");
+        navBar.classList.remove("show");
+        navMenuLinks.forEach((menuLink) => menuLink.removeEventListener("click", closeMobileNavMenu));
+
         activeSection();
         window.addEventListener('scroll', handleScroll);
-        
-    } else {
-        // Reset nav style
+
+    } else { // Mobile nav on
+        // Reset hamburger
+        hamburgerMenu.ariaExpanded = "false"
+        // Reset nav
+        navBar.classList.remove("hide");
+        navBar.classList.remove("show");
         const currentActiveLink = document.querySelector("nav .active");
         (currentActiveLink) ? currentActiveLink.classList.remove("active") : "";
-        navBar.classList.remove("show");
-        navBar.classList.remove("hide");
+        navMenuLinks.forEach((menuLink) => {menuLink.addEventListener("click", closeMobileNavMenu);});
 
         window.removeEventListener('scroll', handleScroll);
     }
 }
 
 mediaQueryWidth.addEventListener("change", handleMediaQueryWidth);
-
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-function handleHamburgerMenu() {
-    if (hamburgerMenu.ariaExpanded === "true") {
-        // Close menu
-        hamburgerMenu.ariaExpanded = "false";
-        navBar.classList.remove("show");
-        navBar.classList.add("hide");
-    } else {
-        // Open menu
-        hamburgerMenu.ariaExpanded = "true";
-        navBar.classList.remove("hide");
-        navBar.classList.add("show")
-    }
-}
-
-hamburgerMenu.addEventListener("click", handleHamburgerMenu)
-
+handleMediaQueryWidth(mediaQueryWidth);
