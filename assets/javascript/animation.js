5ebafc8148c3c0e86f03e03e850de5e51143acc3
Keelin <keelin.baldwinmaher@gmail.com>
2024-11-09 15:36:42 -0700

    UFO animation edits

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -1,27 +1,7 @@
-const canvas = document.getElementById("ufo-canvas");
-const ctx = canvas.getContext("2d");
-
-// function draw() {
-//     canvas.height = ufo.height;
-//     canvas.width = ufo.width;
-
-//     // ufo.src = "/assets/images/ufo/ufo-frame-1.svg";
-//     ctx.scale(0.3, 0.3);
-//     ctx.drawImage(ufo, 0, 0);
-
-// }
-
-// draw();
-
-
-// const duration = 3000;
-
-
-
 const ufoAnimate = {
     duration: 1, // duration in seconds
     loops: false,
-    ufo: document.querySelector(".splash-image img"),
+    ufo: document.querySelector("#ufo"),
     draw(frame) {
         // frame values go from 0 -> 100 in one second
         const fps = 24;
@@ -32,13 +12,14 @@ const ufoAnimate = {
     }
 };
 
+const ufoBorderAnimate = {
+    duration: 3,
+    loops: false,
+    ufoBorder: document.querySelector("#ufo-border")
+};
+
 let timeElapsed = 0; // timeElapsed in seconds
 let start = 0; // start is in milliseconds, because that is what requestAnimationFrame returns
-
-// window.requestAnimationFrame((timeStamp) => {
-//     start = timeStamp;
-//     animate(timeStamp, ufoAnimate);
-// });
 
 function animate(timeStamp, { duration, loops, draw }) {
     let frame = Math.floor(((timeStamp - start) / (duration * 1000)) * 100);
@@ -47,14 +28,15 @@ function animate(timeStamp, { duration, loops, draw }) {
 
     if (frame < 100 && timeElapsed < duration) {
         window.requestAnimationFrame((timeStamp) => animate(timeStamp, { duration, loops, draw }));
-    } else if (frame === 100 && ((timeElapsed <= duration) || loops)) {
-        timeElapsed++;
-        if ((timeElapsed >= duration) && loops) {
-            timeElapsed = 0; // Restart loop
-        }
 
-        start = timeStamp; // Start new second
-        animate(timeStamp, { duration, loops, draw });
+    } else if (frame === 100 && ((timeElapsed <= duration) || loops)) {
+        // timeElapsed++;
+        // if ((timeElapsed >= duration) && loops) {
+        //     timeElapsed = 0; // Restart loop
+        // }
+
+        // start = timeStamp; // Start new second
+        // animate(timeStamp, { duration, loops, draw });
     }
 }
 
