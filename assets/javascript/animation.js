f1c413a0bab838a64514b1b62f73228ba69d5dd1
Keelin <keelin.baldwinmaher@gmail.com>
2024-11-09 18:39:28 -0700

    Messing around with the UFO animation
    Think I figured out how to do step/frame timing.
    The duration is divided into how many steps/frames I want.
    duration / (how many steps) = interval
    current time / interval = frame

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -1,44 +1,43 @@
+function animate(animateObject) {
+    let start = performance.now();
+
+    requestAnimationFrame(function drawNextFrame(time) {
+        let currentTime = (time - start);
+
+        if (currentTime > animateObject.duration) {
+            currentTime = animateObject.duration;
+        }
+
+        let imageState = animateObject.timing(currentTime);
+
+        animateObject.draw(imageState);
+
+        if (currentTime < animateObject.duration) {
+            requestAnimationFrame(drawNextFrame);
+        } else if (currentTime >= animateObject.duration && animateObject.loop) {
+            animate(animateObject);
+        }
+    });
+    
+}
+
 const ufoAnimate = {
-    duration: 1, // duration in seconds
-    loops: false,
     ufo: document.querySelector("#ufo"),
-    draw(frame) {
-        // frame values go from 0 -> 100 in one second
-        const fps = 24;
-        // frame = Math.floor((frame / 100) * fps);
-        // frame = (frame % 8) + 1;
-        console.log(frame);
-        ufo.src = `/assets/images/ufo/ufo-frame-${1}.svg`;
-    }
-};
-
-const ufoBorderAnimate = {
-    duration: 3,
-    loops: false,
-    ufoBorder: document.querySelector("#ufo-border")
-};
-
-let timeElapsed = 0; // timeElapsed in seconds
-let start = 0; // start is in milliseconds, because that is what requestAnimationFrame returns
-
-function animate(timeStamp, { duration, loops, draw }) {
-    let frame = Math.floor(((timeStamp - start) / (duration * 1000)) * 100);
-
-    draw(frame);
-
-    if (frame < 100 && timeElapsed < duration) {
-        window.requestAnimationFrame((timeStamp) => animate(timeStamp, { duration, loops, draw }));
-
-    } else if (frame === 100 && ((timeElapsed <= duration) || loops)) {
-        // timeElapsed++;
-        // if ((timeElapsed >= duration) && loops) {
-        //     timeElapsed = 0; // Restart loop
-        // }
-
-        // start = timeStamp; // Start new second
-        // animate(timeStamp, { duration, loops, draw });
+    duration: 1000,
+    loop: false,
+    timing(currentTime) {
+        if (currentTime > 1000 || currentTime <= 0) {
+            return 1;
+        } else {
+            return parseInt(currentTime / 125);
+        }
+    },
+    draw(imageState) {
+        ufo.src = `/assets/images/ufo/ufo-frame-${imageState}.svg`;
     }
 }
+
+animate(ufoAnimate);
 
 const ufo = document.querySelector("#ufo");
 let currentFrame = 1;
@@ -51,4 +50,4 @@ function animateFrame() {
     ufo.src = ufo.src = `/assets/images/ufo/ufo-frame-${currentFrame}.svg`;
 }
 
-setInterval(animateFrame, 72);
+//setInterval(animateFrame, 72);
